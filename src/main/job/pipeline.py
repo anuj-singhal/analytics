8fdacfb47e685d5abd663dbe60de8f80eb35bcf3
anuj-singhal <62522752+anuj-singhal@users.noreply.github.com>
2021-03-06 09:37:08 +0400

    Add unimplemented methods
    Add unimplemented methods

--- src/main/job/pipeline.py
+++ src/main/job/pipeline.py
@@ -6,17 +6,36 @@ import pyspark.sql.functions as F
 class PySparkJob(PySparkJobInterface):
 
     def init_spark_session(self) -> SparkSession:
-        # TODO: put your code here
-        ...
+        # Initialize Spark Session
+        sparkSession = SparkSession.builder \
+                      .appName("Covid19 Vaccination Progress") \
+                      .getOrCreate()
+        sparkSession.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+        return sparkSession
 
     def count_available_vaccines(self, vaccines: DataFrame) -> int:
-        # TODO: put your code here
-        ...
+        # count number of unique vaccines around the world
+        num = vaccines.select('vaccines').dropDuplicates().count()
+        return num
 
     def find_earliest_used_vaccine(self, vaccines: DataFrame) -> str:
-        # TODO: put your code here
-        ...
+        # earliest vaccine which has been used in the world
+
+        # convert date to DateType
+        vaccines = vaccines.withColumn('date', F.from_unixtime(F.unix_timestamp(vaccines['date'], 'd/M/yy')).cast(DateType()))
+        # sort dataset based on date in ascending order to get the earliest vaccine information
+        df_vaccine = vaccines.orderBy(F.col("date"), F.col('vaccines')).select("vaccines").collect()
+        # get the first vaccine from the dataset
+        earliestVaccine = df_vaccine[0].asDict()["vaccines"]
+        return earliestVaccine
+
 
     def total_vaccinations_per_country(self, vaccines: DataFrame) -> DataFrame:
-        # TODO: put your code here
-        ...
+        # Get the total vaccine available for each country
+
+        # filter total vaccinations where data not available
+        vaccines = vaccines.filter(F.col('total_vaccinations') > 0)
+
+        # Aggregate dataset based on country and get the sum of total vaccines
+        df_total = vaccines.groupBy("country").agg(F.sum("total_vaccinations").alias('total_vaccinations'))
+        return df_total
